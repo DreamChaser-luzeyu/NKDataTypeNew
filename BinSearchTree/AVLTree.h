d77c5631eadad68a4d2dfa98fbb477ec6846d251
DreamChaser <yydluzeyu@gmail.com>
2021-12-04 09:17:12 +0800

    H8

--- BinSearchTree/AVLTree.h
+++ BinSearchTree/AVLTree.h
@@ -7,7 +7,7 @@ using std::istream;
 using std::ostream;
 using std::endl;
 
-enum TYPE{LL, LR, RR, RL};
+enum TYPE{LL, LR, RR, RL, SKIP};
 
 template <class K, class V>
 class AVLNode {
@@ -17,6 +17,10 @@ public:
     AVLNode<K, V>* left;
     AVLNode<K, V>* right;
     int bf = 0;
+    friend ostream& operator<<(ostream& out, AVLNode<K, V> node) {
+        out << "<" << node.weight << "," << node.element << ">";
+        return out;
+    }
 };
 
 template <class K, class V>
@@ -25,16 +29,18 @@ class AVLTree {
 public:
     AVLNode<K, V>* root = nullptr;
     void insert(AVLNode<K, V>* node);
-    AVLNode<K, V>* find(K key);
-
+    bool find(K key, PNODE& p, PNODE& pp);
+    void showTree(ostream& out) const;
 private:
-    bool insertBST(PNODE node, PNODE& pa, PNODE& ppa);                                 // return if the height of a tree changed
+    bool insertBST(PNODE node, PNODE& pa, PNODE& ppa);      // return if the height of a tree changed
+                                                            // 通过引用获得a节点及其父节点
     TYPE getType(PNODE pa);
     void updateBF(PNODE pa, PNODE node);
     void processLL(PNODE x, PNODE px);
     void processLR(PNODE x, PNODE px);
     void processRR(AVLNode<K, V>* x, AVLNode<K, V>* px);
     void processRL(AVLNode<K, V>* x, AVLNode<K, V>* px);
+    void showTree(PNODE root, ostream& out, int level) const;
 };
 
 template <class K, class V>
@@ -45,14 +51,17 @@ void AVLTree<K, V>::insert(AVLNode<K, V>* node) {
     // Get a, pa, isTaller and insert as BST
     bool isTaller = insertBST(node, pa, ppa);
 
+    // if(pa != nullptr)
+    // {std::cout << "a node : " << *pa << endl;}
+
     // 没变高，不影响AVL平衡
-    if(!isTaller) {return;}
+    if(!isTaller) {/*std::cout << "Not taller" << endl;*/ return;}
     // 变高了，先更新平衡因子，为“旋转”做准备
     else {updateBF(pa, node);}
 
     // Maybe node_a does not exist, for example, everything is balanced
     // 只在插入根节点时成立：树变高，且不存在a
-    if(pa == nullptr) {std::cout << "balance ? " << endl; return;}
+    if(pa == nullptr) {/*std::cout << "Cannot find node A" << endl;*/ return;}
 
     // 进行旋转
     TYPE type = getType(pa);
@@ -73,36 +82,57 @@ void AVLTree<K, V>::insert(AVLNode<K, V>* node) {
 }
 
 template <class K, class V>
+bool AVLTree<K, V>::find(K key, PNODE& p, PNODE& pp) {
+    p = root;
+    while(p != nullptr) {
+        pp = p;
+        if(p->weight == key) {return true;}
+        if(p->weight < key) {p = p->right;}
+        else {p = p->left;}
+    }
+    return false;
+}
+
+template <class K, class V>
+void AVLTree<K, V>::showTree(ostream& out) const {
+    showTree(root, out, 0);
+}
+
+template <class K, class V>
 bool AVLTree<K, V>::insertBST(PNODE node, PNODE& pa, PNODE& ppa) {  // 同时还插入了平衡因子
     bool isTaller;
     if(root == nullptr) {root = node; return true;}                 // 插入根节点
     PNODE pp = nullptr;
-    PNODE p = node;
-    while(p) {
-        if((p->bf == 1) || (p->bf == -1)) {pa = p; ppa = pp;}     // get node A
-        pp = p;                                            // update parent of p
+    PNODE p = root;
+
+    while(p != nullptr) {                                           // 找到要插入位置的parent
+        if((p->bf == 1) || (p->bf == -1)) {pa = p; ppa = pp;}       // get node A
+        pp = p;                                                     // update parent of p
         if(node->weight < p->weight) {p = p->left;}
         else if(node->weight > p->weight) {p = p->right;}
-        else {p->element = node->element; return false;}      // 重复的Key 总之在这里要return
+        else {p->element = node->element; return false;}            // 重复的Key 总之在这里要return
     }
 
+    // 判断树是否变高
+    isTaller = pp->bf == 0;                             // better implement
+    // 进行插入节点操作
     // 没变高的情况， 只调整父节点平衡因子
-    if(node->element < pp->element) {
+    if(node->weight < pp->weight) {
         pp->left = node;
-        isTaller = (pp->right) == nullptr;                  // if no brother, then tree higher
+        // isTaller = (pp->right) == nullptr;                  // if no brother, then tree higher
+        if(!isTaller) {pp->bf ++;}                          // if tree is not taller, then bf ++
     }
     else {
         pp->right = node;
-        isTaller = (pp->left) == nullptr;
+        if(!isTaller) {pp->bf --;}
     }
 
-    // 调整父节点平衡因子
-    if(!isTaller) {pp->bf = 0;}
     return isTaller;
 }
 
 template <class K, class V>
 TYPE AVLTree<K, V>::getType(PNODE pa) {
+    if(pa == nullptr) {return SKIP;}
     TYPE type; 
     if(pa->bf == 2) {
         if(pa->left == nullptr) {throw "getType : pa->left is nullptr";}
@@ -114,50 +144,124 @@ TYPE AVLTree<K, V>::getType(PNODE pa) {
         if(pa->right->bf == 1) {type = RL;} 
         else if(pa->right->bf == -1){type = RR;} 
         else {throw "getType : pa->bf->right error";}
-    } else {throw "getType : pa->bf error";}
+    } else {/*throw "getType : pa->bf error";*/ return SKIP;}
     return type;
+}
+
+template <class K, class V>
+void AVLTree<K, V>::updateBF(PNODE beginNode, PNODE node) {
+    PNODE p = beginNode;
+    if(beginNode == nullptr) {p = root;}
+    // K weight = beginNode->weight;
+    while(p != node) {
+        if(node->weight < p->weight) {             // node inserted at p->left, height ++, bf ++
+            p->bf ++;
+            p = p->left;
+        } else {
+            p->bf --;
+            p = p->right;
+        }
+        // pp = p;
+        // weight = pp->weight;
+    }
 }
 
 template <class K, class V>
 void AVLTree<K, V>::processLL(PNODE x, PNODE px) {
     PNODE xl, xr, xll, xlr;
     xl = x->left; xr = x->right;
-    xll = xl->left; xlr = xlr->right;
+    xll = xl->left; xlr = xl->right;
     if(px != nullptr) {
         if(xl->weight < px->weight) {px->left = xl;}
         else {px->right = xl;}
-    }
+    } else {root = xl;} 
     x->left = xlr;
-    x->right = xr;
-    xl->left = xll;
+    xl->right = x;
+
+    xl->bf = 0; x->bf = 0;
 }
 
 template <class K, class V>
 void AVLTree<K, V>::processRR(PNODE x, PNODE px) {
-    PNODE xl, xr, xll, xlr;
+    PNODE xl, xr, xrl, xrr;
     xl = x->left; xr = x->right;
-    xll = xl->left; xlr = xlr->right;
+    xrl = xr->left; xrr = xr->right;
     if(px != nullptr) {
-        if(xl->weight < px->weight) {px->left = xl;}
-        else {px->right = xl;}
-    }
-    x->left = xlr;
-    x->right = xr;
-    xl->left = xll;
+        if(xr->weight < px->weight) {px->left = xr;}
+        else {px->right = xr;}
+    } else {root = xr;}
+    x->right = xrl;
+    xr->left = x;
+
+    xr->bf = 0; x->bf = 0;
 }
 
 template <class K, class V>
 void AVLTree<K, V>::processLR(PNODE x, PNODE px) {
-    PNODE xl, xr, xll, xlr;
+    PNODE xl, xr, xll, xlr, xlrr, xlrl;
     xl = x->left; xr = x->right;
-    xll = xl->left; xlr = xlr->right;
+    xll = xl->left; xlr = xl->right;
+    xlrr = xlr->right; xlrl = xlr->left;
     if(px != nullptr) {
         if(xlr->weight < px->weight) {px->left = xlr;}
         else {px->right = xlr;}
+    } else {root = xlr;}
+    // xl->left = xll;
+    xlr->left = xl;
+    xlr->right = x;
+    xl->right = xlrl;
+    x->left = xlrr;
+
+    switch(xlr->bf) {
+        case -1:
+            xl->bf = 1; x->bf = 0; xlr->bf = 0;
+            break;
+        case 0:
+            xl->bf = 0; x->bf = 0; xlr->bf = 0;
+            break;
+        case 1:
+            xl->bf = 0; x->bf = -1; xlr->bf = 0;
+            break;
     }
-    xl->left = xll;
-    xl->right = xlr->left;
-    x->left = xlr->right;
+}
+
+template <class K, class V>
+void AVLTree<K, V>::processRL(PNODE x, PNODE px) {
+    PNODE xl, xr, xrl, xrr, xrll, xrlr;
+    xl = x->left; xr = x->right;
+    xrl = xr->left; xrr = xr->right;
+    xrll = xrl->left; xrlr = xrl->right;
+    if(px != nullptr) {
+        if(xrl->weight < px->weight) {px->left = xrl;}
+        else {px->right = xrl;}
+    } else {root = xrl;}
+    
+    xrl->right = xr;
+    xrl->left = x;
+    // xr->right = xrr;
+    xr->left = xrlr;
+    x->right = xrll;
+    switch(xrl->bf) {
+        case -1:
+            xr->bf = 0; x->bf = 1; xrl->bf = 0;
+            break;
+        case 0:
+            xr->bf = 0; x->bf = 0; xrl->bf = 0;
+            break;
+        case 1:
+            xr->bf = -1; x->bf = 0; xrl->bf = 0;
+            break;
+    }
+}
+
+template <class K, class V>
+void AVLTree<K, V>::showTree(PNODE root, ostream& out, int level) const {
+    if(root == nullptr) {return;}
+    showTree(root->right, out, level+1);
+    out << endl;
+    for(int i=0; i<level; i++) {out << "        ";}
+    out << "<" << root->weight << "," << root->element << "," << root->bf << ">";
+    showTree(root->left, out, level+1); 
 }
  
 
---
+++ homework/H8-AVLUtils.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include "../BinSearchTree/AVLTree.h"
+
+using std::istream;
+using std::ostream;
+using std::endl;
+using std::string;
+using std::queue;
+
+
+/*
+test LL
+insert 10 10 insert 9 9 insert 8 8
+test RR
+insert 10 10 insert 11 11 insert 12 12
+test LR
+insert 10 10 insert 5 5 insert 15 15
+insert 3 3 insert 7 7
+insert 6 6
+test RL
+insert 10 10 insert 5 5 insert 15 15
+insert 13 13 insert 17 17
+insert 14 14
+
+
+
+*/
+
+/*
+测试树1
+insert 1 1
+insert 5 5
+insert 10 10 
+insert 3 3
+insert 2 2
+insert 15 15
+insert 9 9
+insert 13 13
+insert 14 14
+insert -2 -2
+insert -3 -3
+*/
+
+/*
+测试树2
+insert 5 5
+insert 3 3 insert 8 8
+insert 2 2 insert 4 4 insert 7 7 insert 9 9
+insert 1 1 insert 6 6 insert 10 10
+*/
+
+
+
+template <class K, class V>
+int findHeight(AVLNode<K, V>* beginNode) {      // O(log n)
+    int height = 0;
+    while(beginNode != nullptr) {
+        if(beginNode->bf >= 0) {
+            beginNode = beginNode->left;
+        } else {
+            beginNode = beginNode->right;
+        }
+        height ++;
+    }
+    return height;
+}
+
+template <class K, class V>
+AVLNode<K, V>* findNearestLeaf(AVLNode<K, V>* root) {
+    queue<AVLNode<K, V>* > q;
+    q.push(root);
+    while(!q.empty()) {
+        int sizeNow = q.size();
+        for(int i=0; i<sizeNow; i++) {
+            AVLNode<K, V>* front = q.front();
+            q.pop();
+            if(front == nullptr) {continue;}
+            if(front->left == nullptr && front->right == nullptr) {
+                return front;
+            }
+            q.push(front->left);
+            q.push(front->right);
+        }
+    }
+    return nullptr;
+}
+
+
+template <class K, class V>
+void avlCmd(istream& in, ostream& out) {
+    typedef AVLNode<K, V>* PNODE;
+    out << "insert [key] [val]" << endl
+        << "find [key]" << endl
+        << "height" << endl
+        << "findNeareset" << endl
+        << "showTree" << endl;
+    AVLTree<K, V> avlTree;
+    while(true) {
+        string cmd; in >> cmd;
+        if(cmd == "insert"){
+            K key; V val; in >> key >> val;
+            AVLNode<K, V>* node = new AVLNode<K, V>;
+            node->weight = key; node->element = val;
+            avlTree.insert(node);
+        }else if(cmd == "find") {
+            K key; in >> key; PNODE p; PNODE pp;
+            if(avlTree.find(key, p, pp)) {
+                out << *p;
+            } else {
+                out << "Not found, available parent : " << *pp;
+            }
+        } else if(cmd == "showTree") {
+            avlTree.showTree(out);
+            out << endl;
+        } else if(cmd == "height") {
+            out << "Height of AVL : " << findHeight<K, V>(avlTree.root);
+            out << endl;
+        } else if(cmd == "findNearest") {
+            PNODE ptr = findNearestLeaf(avlTree.root);
+            if(ptr == nullptr) {out << "Error" << endl;}
+            out << *ptr << endl;
+        } else {
+            out << "Wrong command" << endl;
+        }
+    }
+}
+
+int main() {
+    avlCmd<int, string>(std::cin, std::cout);
+    return 0;
+}
